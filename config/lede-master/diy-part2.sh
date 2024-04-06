f5d5164c768c08b6f44f4fb39dff38c6f3bfe08e
aenstan <54591257+aenstan@users.noreply.github.com>
2024-04-06 11:40:01 +0800

    Update diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -41,11 +41,11 @@ sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # Add third-party software packages (The entire repository)
 # git clone https://github.com/libremesh/lime-packages.git package/lime-packages
-find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-find ./ | grep Makefile | grep mosdns | xargs rm -f
-git clone https://github.com/sbwml/luci-app-mosdns.git -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata.git package/v2ray-geodata
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+#find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+#find ./ | grep Makefile | grep mosdns | xargs rm -f
+#git clone https://github.com/sbwml/luci-app-mosdns.git -b v5 package/mosdns
+#git clone https://github.com/sbwml/v2ray-geodata.git package/v2ray-geodata
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
 
